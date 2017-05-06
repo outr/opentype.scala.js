65958e65100b1b25857d11ca1b0892548fc34a78
Matt Hicks <matt@outr.com>
2017-05-05 20:45:59 -0400

    Fully functionality integration of facade.

--- src/main/scala/opentype/Example.scala
+++ src/main/scala/opentype/Example.scala
@@ -9,8 +9,26 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Example {
   @JSExportTopLevel("example")
   def main(): Unit = {
+    val canvas = document.createElement("canvas").asInstanceOf[html.Canvas]
+    canvas.width = 800
+    canvas.height = 600
+    canvas.style.border = "1px solid black"
+    document.body.appendChild(canvas)
+
+    val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+
     OpenType.load("fonts/Roboto-Black.ttf").map { font =>
-      println(s"Font: ${font.ascender}, ${font.descender}")
+      val path = font.getPath("Hello, World!", 50.0, 150.0, 72.0)
+      path.fill = "blue"
+      path.draw(ctx)
+    }
+
+    OpenType.load("fonts/FiraSansMedium.woff").map { font =>
+      val path = font.getPath("Hello, World!", 50.0, 250.0, 72.0)
+      path.fill = "green"
+      path.stroke = "black"
+      path.strokeWidth = 1.5
+      path.draw(ctx)
     }
   }
 }
--- src/main/scala/opentype/Font.scala
+++ src/main/scala/opentype/Font.scala
@@ -1,11 +1,62 @@
 package opentype
 
+import org.scalajs.dom.raw.CanvasRenderingContext2D
+
 import scala.scalajs.js
-import scala.scalajs.js.annotation.{JSGlobal, JSName}
+import scala.scalajs.js.annotation.{JSGlobal, JSName, ScalaJSDefined}
 
 @js.native
 @JSGlobal("opentype.Font")
 class Font extends js.Object {
+  def glyphs: js.Array[Glyph] = js.native
+  def unitsPerEm: Double = js.native
   def ascender: Double = js.native
   def descender: Double = js.native
+
+  def getPath(text: String,
+              x: Double = js.native,
+              y: Double = js.native,
+              fontSize: Double = js.native,
+              options: PathOptions = js.native): Path = js.native
+  def getPaths(text: String,
+               x: Double = js.native,
+               y: Double = js.native,
+               fontSize: Double = js.native,
+               options: PathOptions = js.native): js.Array[Path] = js.native
+  def draw(ctx: CanvasRenderingContext2D,
+           text: String,
+           x: Double = js.native,
+           y: Double = js.native,
+           fontSize: Double = js.native,
+           options: PathOptions = js.native): Path = js.native
+  def drawPoints(ctx: CanvasRenderingContext2D,
+                 text: String,
+                 x: Double = js.native,
+                 y: Double = js.native,
+                 fontSize: Double = js.native,
+                 options: PathOptions = js.native): Path = js.native
+  def drawMetrics(ctx: CanvasRenderingContext2D,
+                  text: String,
+                  x: Double = js.native,
+                  y: Double = js.native,
+                  fontSize: Double = js.native,
+                  options: PathOptions = js.native): Path = js.native
+  def stringToGlyphs(text: String): js.Array[Glyph] = js.native
+  def charToGlyph(char: Char): Glyph = js.native
+  def getKerningValue(leftGlyph: Glyph, rightGlyph: Glyph): Double = js.native
+  def getAdvanceWidth(text: String, fontSize: Double = js.native, options: PathOptions = js.native): Double = js.native
+  def download(): Unit = js.native
+}
+
+@ScalaJSDefined
+trait PathOptions extends js.Any {
+  var kerning: js.UndefOr[Boolean] = js.undefined
+  var features: js.UndefOr[OpenTypeFeatureTags] = js.undefined
+  var hinting: js.UndefOr[Boolean] = js.undefined
+}
+
+@ScalaJSDefined
+trait OpenTypeFeatureTags extends js.Any {
+  var liga: js.UndefOr[Boolean] = js.undefined
+  var rlig: js.UndefOr[Boolean] = js.undefined
 }
